ce99da61169ce0594594ca9ac2109820d3f5544e
Ewerton <ewerton.pluri@gmail.com>
2021-09-19 20:16:03 -0300

    refact: detail page integration

--- src/app/pages/bot-details/bot-details.component.ts
+++ src/app/pages/bot-details/bot-details.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { AnalyticsInterface } from 'src/app/shared/interfaces/analytics.interface';
+import { Component, OnInit } from '@angular/core';
+import { ACTIVE_USERS, BOT_REGION, RECEIVED_MESSAGES, SENT_MESSAGES } from 'src/app/shared/constants/analytics';
+import { AnalyticsMessageInterface, AnalyticsUserInterface } from 'src/app/shared/interfaces/analytics.interface';
 import { BotInterface } from 'src/app/shared/interfaces/bot.interface';
 import { BotDetailsService } from 'src/app/shared/services/bot-details.service';
 
@@ -10,6 +11,12 @@ import { BotDetailsService } from 'src/app/shared/services/bot-details.service';
 })
 export class BotDetailsComponent implements OnInit {
   public botInfo: BotInterface;
+  public botMessages: AnalyticsMessageInterface;
+  public activeUsers: AnalyticsUserInterface;
+  public SENT_MESSAGES= SENT_MESSAGES;
+  public RECEIVED_MESSAGES = RECEIVED_MESSAGES;
+  public ACTIVE_USERS = ACTIVE_USERS;
+  public BOT_REGION = BOT_REGION;
 
   constructor(private botDetailsService: BotDetailsService) {
     this.getBotInformation();
@@ -20,6 +27,8 @@ export class BotDetailsComponent implements OnInit {
 
   getBotInformation() {
     this.botInfo = this.botDetailsService.getBot();
+    this.botMessages = this.botInfo.analytics.message;
+    this.activeUsers = this.botInfo.analytics.user;
   }
 
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { BotInterface } from 'src/app/shared/interfaces/bot.interface';
+import { BotDetailsService } from 'src/app/shared/services/bot-details.service';
 import { BotManagerService } from 'src/app/shared/services/bot-manager.service';
 import { BLOCKS_VIEW, LIST_VIEW } from '../../shared/constants/layout';
 
@@ -11,11 +13,15 @@ import { BLOCKS_VIEW, LIST_VIEW } from '../../shared/constants/layout';
 export class HomeComponent implements OnInit {
   public favoriteBots: BotInterface[];
   public notFavoriteBots: BotInterface[];
-  public layoutMode: string = BLOCKS_VIEW; //mudar
+  public layoutMode: string = BLOCKS_VIEW;
   public blockMode: string = BLOCKS_VIEW;
   public listMode: string = LIST_VIEW;
 
-  constructor(private botManagerService: BotManagerService) {}
+  constructor(
+    private botManagerService: BotManagerService,
+    private botDetailsService: BotDetailsService,
+    private router: Router
+    ) {}
 
   ngOnInit(): void {
     this.botManagerService.getNotFavoriteBots()
@@ -26,14 +32,15 @@ export class HomeComponent implements OnInit {
   }
 
   toggleFavorite(newValue: any, bot: BotInterface) {
-    if (newValue.isFavorite) {
-      this.botManagerService.changeFavoriteState(bot, true);
-    } else {
-      this.botManagerService.changeFavoriteState(bot, false);
-    }
+      this.botManagerService.changeFavoriteState(bot, newValue.isFavorite);
   }
 
   changeLayout(newMode: string) {
     this.layoutMode = newMode;
+  }
+
+  openDetails(selectedBot: BotInterface) {
+    this.botDetailsService.setBot(selectedBot);
+    this.router.navigateByUrl('details');
   }
 }
